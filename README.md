c415f76b059db96f25f5f772a2c2a0091fa2e980
Alex Lake-Smith <alakesmi@TVNZ2020644.local>
2022-03-22 11:10:49 +1300

    First commit of course repo.

---
+++ README.md
@@ -0,0 +1,2 @@
+First course on TS. Beginner oriented.
+Course content from: https://www.youtube.com/watch?v=BwuLxPH8IDs
---
+++ app.ts
@@ -0,0 +1,10 @@
+// Core data types.
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+const num1 = 2;
+const num2 = 4;
+
+console.log(add(num1, num2));
