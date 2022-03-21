5f92bf336e1e6699796935e32961c0aaa673d110
Alex Lake-Smith <alakesmi@TVNZ2020644.local>
2022-03-22 11:27:05 +1300

    Understanding how to correctly check tpyes in functions using TS to avoid runtime errors.

--- app.ts
+++ app.ts
@@ -1,10 +1,15 @@
 // Core data types.
 
-function add(n1, n2) {
-  return n1 + n2;
+// By adding the : number/boolean after the param TS checks that both of the passed vars are the correct type.
+// Catching the error before it is even compiled, but the compiler will also alert you.
+function add(n1: number, n2: number, tf: boolean) {
+  if (tf) {
+    return n1 + n2;
+  }
 }
 
 const num1 = 2;
 const num2 = 4;
+const printResult = true;
 
-console.log(add(num1, num2));
+console.log(add(num1, num2, printResult));
