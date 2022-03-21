1608f8b86301eef3ee415940f5b4106b84ff6a7b
Alex Lake-Smith <alakesmi@TVNZ2020644.local>
2022-03-22 12:32:50 +1300

    Messing around with Tuples and Arrays.

--- app.ts
+++ app.ts
@@ -1,17 +1,30 @@
 // These {} are TS's notation of specialised object types.
 // It allows us to give information on the structure of an object.
-// You can do this, but you can also just let the inferencer figure it out for us.
-/*const person: {
+// You can do this, but you can also just let the inferencer figure it out for us (unless using tuples).
+const person: {
   name: string;
   age: number;
+  hobbies: string[];
+  // declaring a tuple type. Special
+  role: [number, string];
 } = {
   name: "Alex",
   age: 20,
-};*/
-
-const person = {
-  name: "Alex",
-  age: 20,
+  hobbies: ["Gaming", "Sports"],
+  role: [10, "Boss"],
 };
 
+// This allows non strict array entries as opposed to defining favouriteThings:string[] which would only allow strings.
+// Don't want to use often as you lose the benefits TS gives you and brings you back to the world of JS.
+let favouriteThings: string[];
+favouriteThings = ["Cooking"];
+
 console.log(person);
+console.log(favouriteThings);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase);
+  // TS allows us to manipulate hobby correctly because it knows it is a string so can do the above command.
+  // It also knows that string cannot be mapped so will not allow the following command.
+  //console.log(hobby.map())
+}
