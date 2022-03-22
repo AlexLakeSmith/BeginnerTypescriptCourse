80b1105170c5d33750c0565e900fcf17d79834fe
Alex Lake-Smith <alakesmi@TVNZ2020644.local>
2022-03-22 15:55:59 +1300

    Unpacked function types and callbacks.

--- app.ts
+++ app.ts
@@ -9,3 +9,24 @@ function printResults(num: number) {
 }
 
 printResults(addNums(1, 1));
+
+// This could work. By setting a pointer to addNums you could technically use combineValues with the same functionality.
+// But if the let value is changed, it will not work.
+// We can assign the expected type to use the the Function keyword, to ensure it is not overwritten by some other data type.
+// Although it could end up pointing to a different function if assigned later on. So instead of:
+//let combineValues: Function;
+//You can use
+let combineValues: (a: number, b: number) => number;
+// This is saying that it should accept any function that takes 2 params where both are nums and the function returns a number.
+combineValues = addNums;
+console.log(combineValues(2, 9));
+
+// Added functionality with callback functions.
+function addAndHandle(n1: number, n2: number, cb: (t: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndHandle(10, 20, (result) => {
+  console.log("Result is " + result);
+});
